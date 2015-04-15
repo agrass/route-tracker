19e22629f77eab582fdb4896b6407fbb192a35f6
Antonio Grass <antonio_grass@hotmail.com>
2015-04-15 11:54:53 -0300

    add route-tracker first commit

---
+++ lib/route-tracker.rb
@@ -0,0 +1,25 @@
+module RouteTracker
+  module RouteTrackeable
+    extend ActiveSupport::Concern
+ 
+    included do
+    end
+ 
+    module ClassMethods
+      def route_trackeable(options = {})
+        cattr_accessor :level
+        self.level = (options[:level] || "level").to_s
+ 
+        include RouteTracker::RouteTrackeable::LocalInstanceMethods
+      end
+    end
+ 
+    module LocalInstanceMethods
+      def check_level(request)
+        write_attribute(self.class.level, request)
+      end
+    end
+  end
+end
+ 
+ActiveRecord::Base.send :include, RouteTracker::RouteTrackeable
